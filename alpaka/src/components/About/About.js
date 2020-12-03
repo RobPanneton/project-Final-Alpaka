fba529448221cdb28f22cc033ac41ab5235a3452
RobPanneton <robert.panneton@gmail.com>
2020-12-03 09:43:35 -0500

    add content to about page

--- alpaka/src/components/About/About.js
+++ alpaka/src/components/About/About.js
@@ -4,28 +4,83 @@ import styled from "styled-components";
 const About = () => {
   return (
     <Wrapper>
-      <Alpaka>ALPAKA: about page</Alpaka>
+      <AboutWrapper>
+        <p>
+          Founded in 2017, we are an event production company focused on
+          providing high quality events in Montreal's underground electronic
+          music scene. Producers of Fragments, Collision and Pulsar events.
+        </p>
+      </AboutWrapper>
+      <DividerDiv></DividerDiv>
+      <ContactWrapper>
+        <form>
+          <label>
+            Full Name:
+            <input type="text" name="name" placeholder="Your Name" />
+          </label>
+          <label>
+            Email
+            <input type="text" name="email" placeholder="Your Email" />
+          </label>
+          <textarea
+            name="yourMessage"
+            aria-invalid="false"
+            placeholder="Your Message"
+            rows="10"
+            cols="40"
+          />
+          <input type="submit" name="submitForm" value="Submit Form" />
+        </form>
+      </ContactWrapper>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   background-color: black;
+  padding: 74px 90px 0 90px;
   width: 100%;
   height: 600px;
   text-align: center;
   display: flex;
-  margin: 0 auto;
-  align-items: center;
-  justify-content: center;
 `;
 
-const Alpaka = styled.h1`
-  font-size: 100px;
-  background: -webkit-linear-gradient(#eee, #333);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
+const AboutWrapper = styled.div`
+  width: 40%;
+
+  p {
+    color: #eaeaee;
+    font-size: 32px;
+    line-height: 1.5;
+    text-align: left;
+  }
+`;
+
+const DividerDiv = styled.div`
+  width: 20%;
+`;
+
+const ContactWrapper = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: right;
+
+  input {
+    background-color: #eaeaee;
+    color: black;
+  }
+
+  textarea {
+    background-color: #eaeaee;
+    color: black;
+  }
+
+  form {
+    padding-top: 90px;
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export default About;
