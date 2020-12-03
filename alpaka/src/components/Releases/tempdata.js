71d67658ec1cf76120b0e83e0c200549ba7842ef
RobPanneton <robert.panneton@gmail.com>
2020-12-03 14:55:15 -0500

    set template with temp data

--- alpaka/src/components/Releases/tempdata.js
+++ alpaka/src/components/Releases/tempdata.js
@@ -1,1 +1,86 @@
-export const releases = [{}, {}];
+export const releases = [
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+  {
+    _id: "alpaka001",
+    picUrl:
+      "https://geo-media.beatport.com/image/6ccf9143-b95c-402e-a62e-86ac167b9726.jpg",
+    name: "Ben Coda - Chemical Code",
+  },
+];
