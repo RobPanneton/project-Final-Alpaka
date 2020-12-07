bd720e266761c89bdf0a99a186105638203fc15c
RobPanneton <robert.panneton@gmail.com>
2020-12-07 17:30:01 -0500

    add get and add artist

--- server/handlers/handlers-index.js
+++ server/handlers/handlers-index.js
@@ -46,7 +46,7 @@ const getAboutText = async (req, res) => {
 
     res.status(200).json({
       status: 200,
-      message: "About text data retreived",
+      message: "About text data retrieved",
       data: result,
     });
   } catch (err) {
@@ -59,7 +59,61 @@ const getAboutText = async (req, res) => {
 
   client.close();
 };
+
+const getArtistsContent = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+
+    const db = await client.db("alpaka");
+
+    const result = await db.collection("artists").find().toArray();
+
+    console.log(result);
+
+    res.status(200).json({
+      status: 200,
+      message: "Artists data retrieved",
+      data: result,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 404,
+      message1: "data not found",
+      message2: err.message,
+    });
+  }
+  client.close();
+};
+
+const addArtistContent = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+
+    const db = await client.db("alpaka");
+
+    const result = await db.collection("artists").insertOne(req.body);
+
+    assert.equal(1, result.insertedCount);
+
+    res.status(201).json({
+      status: 201,
+      data: req.body,
+      message: "The artist has been added!",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      data: req.body,
+      message: err.message,
+    });
+  }
+};
+
 module.exports = {
   createAboutText,
   getAboutText,
+  getArtistsContent,
+  addArtistContent,
 };
