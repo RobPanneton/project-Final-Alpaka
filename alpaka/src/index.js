db40471c4c9341fffdc2c56a4ac9875f8118d096
RobPanneton <robert.panneton@gmail.com>
2020-12-07 13:26:53 -0500

    setup

--- alpaka/src/index.js
+++ alpaka/src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
 
+import { Provider } from "react-redux";
+import configureStore from "./store";
+
+const store = configureStore();
+
 ReactDOM.render(
   <StylesProvider injectFirst>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </StylesProvider>,
   document.getElementById("root")
 );
